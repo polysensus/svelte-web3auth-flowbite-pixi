497eeb165f3d96de9fe284537487c4587a6f859d
Robin Bryce <robinbryce@gmail.com>
2023-04-09 23:44:18 +0000

    show two canvas instances

--- src/routes/pixi/+page.svelte
+++ src/routes/pixi/+page.svelte
@@ -3,6 +3,12 @@
     <HelloPixi {x} {y}/>
   </div>
 </PixiCanvas>
+<PixiCanvas canvasId={2}>
+  <div slot="canvas">
+    <HelloPixi {x} {y} text="Hello 2"/>
+  </div>
+</PixiCanvas>
+
 <script>
   import PixiCanvas from "../../lib/components/dev/PixiCanvas.svelte";
   import HelloPixi from "../../lib/components/dev/HelloPixi.svelte";
